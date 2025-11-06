1f16d4b83db189070d256f1e966e3fd6acb228ef
PaulGroza <andreigroza13@gmail.com>
2025-11-06 19:20:58 +0200

    Added Session creation menu

--- frontend/conference-management-system/src/components/CreateConferece/CreateConference.jsx
+++ frontend/conference-management-system/src/components/CreateConferece/CreateConference.jsx
@@ -1,150 +1,178 @@
-import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
-import { CalendarMonth } from '@mui/icons-material';
-import BusinessIcon from '@mui/icons-material/Business';
-import LocationPinIcon from '@mui/icons-material/LocationPin';
-import GroupsIcon from '@mui/icons-material/Groups';
-import DescriptionIcon from '@mui/icons-material/Description';
-import CreateIcon from '@mui/icons-material/Create';
-import LinkIcon from '@mui/icons-material/Link';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { CalendarMonth } from "@mui/icons-material";
+import BusinessIcon from "@mui/icons-material/Business";
+import LocationPinIcon from "@mui/icons-material/LocationPin";
+import GroupsIcon from "@mui/icons-material/Groups";
+import DescriptionIcon from "@mui/icons-material/Description";
+import CreateIcon from "@mui/icons-material/Create";
+import LinkIcon from "@mui/icons-material/Link";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
-import { addIcon } from '../../utils/getIcons';
-import "./CreateConference.css"
+import { addIcon } from "../../utils/getIcons";
+import "./Conference.css";
 
-import DatePicker from 'react-datepicker';
+import DatePicker from "react-datepicker";
 import CreateSession from "./CreateSession";
-import {useState} from "react";
+import { useState } from "react";
 
-const VisuallyHiddenInput = styled('input')({
-    clip: 'rect(0 0 0 0)',
-    clipPath: 'inset(50%)',
-    height: 1,
-    overflow: 'hidden',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    whiteSpace: 'nowrap',
-    width: 1,
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
 });
 
-function CreateConference({isSessionOpen, onOpenSession}) {
+function CreateConference({
+  isSessionOpen,
+  setIsSessionOpen,
+  sessions,
+  setOpenSessionData,
+  setEditingIndex,
+}) {
+  function editSession(session, index) {
+    if (isSessionOpen === false) {
+      setOpenSessionData({
+        title: session.title,
+        startDate: session.startDate,
+        endDate: session.endDate,
+        capacity: session.capacity,
+        room: session.room,
+        description: session.description,
+      });
+      setIsSessionOpen(true);
+      setEditingIndex(index);
+    }
+  }
 
-    // const [sessions, setSessions] = useState([]);
-    // const [sessionIsOpen, setSessioIsOpen] = useState(false);
+  return (
+    <div className={"create-conf-box"}>
+      <h1>Create your conference!</h1>
+      <form
+        className={"create-conference-form"}
+        noValidate={false}
+        autoComplete={"off"}
+      >
+        <div className={"pair"}>
+          <TextField
+            label="Title"
+            {...addIcon(CreateIcon)}
+            fullWidth={true}
+            autoFocus={true}
+          />
 
-    return (
-        // <div className={""}>
-            <div className={"create-conf-box"}>
-                <h1>Create your conference!</h1>
-                <form className={"create-conference-form"} noValidate={false} autoComplete={"off"}>
-                    <div className={"pair"}>
-                        <TextField
-                            label="Title"
-                            {...addIcon(CreateIcon)}
-                            fullWidth={true}
-                            autoFocus={true}
-                        />
+          <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            size={"small"}
+            startIcon={<CloudUploadIcon />}
+          >
+            Upload Image
+            <VisuallyHiddenInput
+              type="file"
+              onChange={(event) => console.log(event.target.files)}
+              multiple
+            />
+          </Button>
+        </div>
 
-                        <Button
-                            component="label"
-                            role={undefined}
-                            variant="contained"
-                            tabIndex={-1}
-                            size={"small"}
-                            startIcon={<CloudUploadIcon />}
-                        >Upload Image
-                            <VisuallyHiddenInput
-                                type="file"
-                                onChange={(event) => console.log(event.target.files)}
-                                multiple
-                            />
-                        </Button>
+        <div className={"pair"}>
+          <TextField
+            fullWidth={true}
+            type={"date"}
+            label={"Start Date"}
+            defaultValue={""}
+            slotProps={{
+              inputLabel: {
+                shrink: true,
+              },
+            }}
+          />
 
-                    </div>
+          <TextField
+            label="End Date"
+            fullWidth={true}
+            type={"date"}
+            slotProps={{
+              inputLabel: {
+                shrink: true,
+              },
+            }}
+          />
+        </div>
 
-                    <div className={"pair"}>
-                        <TextField
-                            fullWidth={true}
-                            type={"date"}
-                            label={"Start Date"}
-                            defaultValue={""}
-                            slotProps={{
-                                inputLabel: {
-                                    shrink:true
-                                }
-                            }}
-                        />
+        <div className={"pair"}>
+          <TextField
+            label="Location Name"
+            fullWidth={true}
+            {...addIcon(BusinessIcon)}
+          />
 
-                        <TextField
-                            label="End Date"
-                            fullWidth={true}
-                            type={"date"}
-                            slotProps={{
-                                inputLabel: {
-                                    shrink:true
-                                }
-                            }}
-                        />
-                    </div>
+          <TextField
+            label="Location Address"
+            fullWidth={true}
+            {...addIcon(LocationPinIcon)}
+          />
+        </div>
 
-                    <div className={"pair"}>
-                        <TextField
-                            label="Location Name"
-                            fullWidth={true}
-                            {...addIcon(BusinessIcon)}
-                        />
+        <div className={"pair"}>
+          <TextField
+            label={"Capacity"}
+            fullWidth={true}
+            type={"number"}
+            {...addIcon(GroupsIcon)}
+          />
 
-                        <TextField
-                            label="Location Address"
-                            fullWidth={true}
-                            {...addIcon(LocationPinIcon)}
-                        />
-                    </div>
+          <TextField
+            label={"Registration Deadline"}
+            fullWidth={true}
+            type={"date"}
+            slotProps={{
+              inputLabel: {
+                shrink: true,
+              },
+            }}
+          />
+        </div>
 
-                    <div className={"pair"}>
-                        <TextField
-                            label={"Capacity"}
-                            fullWidth={true}
-                            type={"number"}
-                            {...addIcon(GroupsIcon)}
-                        />
+        <TextField label="Website" type={"url"} {...addIcon(LinkIcon)} />
 
-                        <TextField
-                            label={"Registration Deadline"}
-                            fullWidth={true}
-                            type={"date"}
-                            slotProps={{
-                                inputLabel: {
-                                    shrink:true
-                                }
-                            }}
-                        />
-                    </div>
-
-                    <TextField
-                        label="Website"
-                        type={"url"}
-                        {...addIcon(LinkIcon)}
-                    />
-
-                    <TextField
-                        label="Description"
-                        multiline
-                        {...addIcon(DescriptionIcon)}
-                    />
-
-                </form>
-                <p>Current Sessions added (click to edit): </p>
-                <button
-                    className={"btn"}
-                    onClick={() => !isSessionOpen ? onOpenSession(!isSessionOpen) : null}
-                >Add New Session</button>
-            </div>
-        // </div>
-    )
+        <TextField
+          label="Description"
+          multiline
+          {...addIcon(DescriptionIcon)}
+        />
+      </form>
+      <p>
+        {`Current Sessions added (click to edit): `}
+        {sessions.length
+          ? sessions.map((session, index) => (
+              <span
+                key={index}
+                onClick={() => editSession(session, index)}
+              >{`${session.title} `}</span>
+            ))
+          : ""}
+      </p>
+      <button
+        className={"btn"}
+        onClick={() =>
+          !isSessionOpen ? setIsSessionOpen(!isSessionOpen) : null
+        }
+      >
+        Add New Session
+      </button>
+    </div>
+  );
 }
 
 export default CreateConference;
